9f03a8450c1f3afad8ed7b93f15a3d4dc8c326f5
pallavipulamolu <151707962+pallavipulamolu@users.noreply.github.com>
2024-08-21 15:16:04 +0530

    Add files via upload

---
+++ type_function.py
@@ -0,0 +1,12 @@
+a = ("Sasi","College","Engineering")
+b = ["Sasi","College","Engineering"]
+c = {"Sasi":1, "College":2, "Engineering":3}
+d = "Hello World"
+e = 10.23
+f = 11.34568
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
