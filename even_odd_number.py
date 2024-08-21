cbf560975a90c116c49088bf04777e6294982835
pallavipulamolu <151707962+pallavipulamolu@users.noreply.github.com>
2024-08-21 15:25:06 +0530

    Add files via upload

---
+++ even_odd_number.py
@@ -0,0 +1,9 @@
+n = int(input('Enter the number'))
+if n%2==0:
+    print(n, "is even number")
+    print("End of if block")
+else:
+    print(n, "is odd number")
+    print("End of else block")
+print("End of the program")
+print("Execution is completed")
