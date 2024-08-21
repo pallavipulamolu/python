4c77ece07ca97bcb12825af7d5489a0a2dddc55a
pallavipulamolu <151707962+pallavipulamolu@users.noreply.github.com>
2024-08-21 14:38:45 +0530

    Add files via upload

---
+++ type.py
@@ -0,0 +1,6 @@
+a = 100
+print("The type of variable having value",a,"is",type(a))
+b = 10.23456789
+print("The type of variable having value",b,"is",type(b))
+c = 100+5j
+print("The type of variable having value",c,"is",type(c))
